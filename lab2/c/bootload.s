92a18b91d42c576b3041e98ed31d9007eec2cd41
joey <zhao305149619@gmail.com>
2019-11-06 08:00:19 +0000

    In real mode, use int12 to get ROM-BIOS memory-size && add REMDME file

--- lab2/c/bootload.s
+++ lab2/c/bootload.s
@@ -1,31 +1,57 @@
-.code16		#16bit的实模式
-.global start	#声明供外部使用
-.text		#定义代码段
-hex:	.ascii	"0123456789ABCDEF"
+	.code16				# 
+
+	.section	.text
+#-----------------------------------------------------------------
 start:
-	ljmp	$0x07c0, $main	#跳转到main这个label
-msg:	.asciz	"Hello World"
-len:	.word 	. - msg		#mes的长度
-main:
-	mov %cs, %ax	#
-	mov %ax, %ds	#
-	mov %ax, %es	#初始化数据段寄存器
+	ljmp	$0x07c0, $main		# renormalize CS and IP
+	#jmp	main			# renormalize CS and IP
+#-----------------------------------------------------------------
+msg:	.ascii	"\n\r Real-Mode Memory: "	# message legend
+buf:	.ascii	"    0 KB \n\r"			# info to report
+len:	.word	. - msg				# message length
+ten:	.word	10			# decimal-system's radix
+#-----------------------------------------------------------------
+main:	# setup segment-registers to address our program data
 
-	mov $0xB800, %ax	#bios-vedio address
-	mov %ax, %es		#0xB800 to es register
-	xor %di, %di		#init %di value to zero
-	movb $'A', %es:0(%di)	#copy 'A' to es segment di(offset) location char
-	movb $0x03, %es:1(%di)	#set the attr (b_display) value to 0x03
+	mov	%cs, %ax		# address thus segment
+	mov	%ax, %ds		#   with DS register
+	mov	%ax, %es		#   also ES register
 
-ax2hex:
-	pusha	#sace general registers
-	mov	$4, %cx
-nxnyb:
-	rol	$4, %ax		#rotate hi-nybble into AL
+	# invoke ROM-BIOS service to obtain memory-size (in KB)
 
-loop:	jmp loop	#死循环
-.org	510
-	.byte	0x55
-	.byte 	0xAA
-#	.word	0xAA55
-.end
+	int	$0x12			# get ram's size into AX
+
+	# use repeated division by ten to convert the binary value 
+	# in AX into a decimal digit-string, without leading zeros
+
+	mov	$5, %di			# initialize array-index
+nxdgt:	
+	xor	%dx, %dx		# extend AX for division
+	divw	ten			# divide by decimal-base
+	add	$'0', %dl		# change number to ascii
+	dec	%di			# move index to the left  
+	mov	%dl, buf(%di)		# and store that numeral
+	or	%ax, %ax		# did quotient equal 0?
+	jnz	nxdgt			# no, get another digit
+
+	# invoke ROM-BIOS video services to display our message
+
+	mov	$0x0F, %ah		# get video-page into BH
+	int	$0x10			# request VIDEO service
+
+	mov	$0x03, %ah 		# cursor (row,col) to DX
+	int	$0x10			# request VIDEO service
+
+	mov	$msg, %bp 		# point ES:BP to string
+	mov	len, %cx		# setup CX with length
+	mov	$0x0D, %bl		# setup BL with colors
+	mov	$0x1301, %ax		# specify write_string
+	int	$0x10			# request VIDEO service
+
+freeze:	jmp	freeze			# spin here until reboot
+#-----------------------------------------------------------------
+	.org	510			# boot-signature's offset
+	.byte	0x55, 0xAA		# value of boot-signature
+#-----------------------------------------------------------------
+	.end				# nothing more to assemble
+
